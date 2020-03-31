34ec980b7aac677c90c8b84cc75248ec1fef38f1
Mitchell Driedger <driedgerm3@mymacewan.ca>
2020-03-31 13:59:12 -0600

    some changes

--- 291CourseProject/AddCar.Designer.cs
+++ 291CourseProject/AddCar.Designer.cs
@@ -156,6 +156,7 @@
             this.button1.TabIndex = 43;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // AddCar
             // 
