ad539481a3495ce67f8234b43e9b7eea9e85c2df
Brandon Douglas <narken@ymail.com>
2020-04-01 21:29:44 -0600

    Finishing customer side program

--- 291CourseProject/CarSelect.cs
+++ 291CourseProject/CarSelect.cs
@@ -18,13 +18,15 @@ namespace _291CourseProject
         DateTime dropoffDate;
         TimeSpan pickupTime;
         TimeSpan dropoffTime;
-        public CarSelect(string selectedBranch, DateTime pickupDate, DateTime dropoffDate, TimeSpan pickupTime, TimeSpan dropoffTime)
+        string Customer_ID;
+        public CarSelect(string selectedBranch, DateTime pickupDate, DateTime dropoffDate, TimeSpan pickupTime, TimeSpan dropoffTime, string userID)
         {
             this.selectedBranch = selectedBranch;
             this.pickupDate = pickupDate;
             this.dropoffDate = dropoffDate;
             this.pickupTime = pickupTime;
             this.dropoffTime = dropoffTime;
+            this.Customer_ID = userID;
             InitializeComponent();
             //sql connection
             string connetionString;
@@ -48,7 +50,7 @@ namespace _291CourseProject
         {
             foreach (Form oForm in Application.OpenForms)
             {
-                if (oForm is Customer)
+                if (oForm is PickupDetails)
                 {
                     oForm.Show();
                     break;
@@ -83,22 +85,13 @@ namespace _291CourseProject
 
             cnn.Open();
             //insert statment
-            string rentalInsert = "INSERT INTO dbo.Rental(Rental_id,Rental_Type,Pickup_Date,Pickup_time,Return_Date,Return_Time) VALUES(@Rental_id,@Rental_Type,@Pickup_Date,@Pickup_time,@Return_Date,@Return_Time)";
-            //insert values
-            SqlCommand rentalInsertcmd = new SqlCommand(rentalInsert, cnn);
-            rentalInsertcmd.Parameters.AddWithValue("@Rental_ID", Rental_ID);
-            rentalInsertcmd.Parameters.AddWithValue("@Rental_Type", 2);//****************edit************************************************
-            rentalInsertcmd.Parameters.AddWithValue("@Pickup_Date", this.pickupDate);
-            rentalInsertcmd.Parameters.AddWithValue("@Pickup_time", this.pickupTime);
-            rentalInsertcmd.Parameters.AddWithValue("@Return_Date", this.dropoffDate);
-            rentalInsertcmd.Parameters.AddWithValue("@Return_Time", this.dropoffTime);
+            SqlCommand rentalInsertcmd = Rental_Insert(cnn, Rental_ID);
 
-            //insert statement
-            string rentedInsert = "INSERT INTO dbo.Rented(Rental_ID,Car_ID) VALUES(@Rental_ID,@Car_ID)";
-            //insert values
-            SqlCommand rentedInsertcmd = new SqlCommand(rentedInsert, cnn);
-            rentedInsertcmd.Parameters.AddWithValue("@Rental_ID", Rental_ID);
-            rentedInsertcmd.Parameters.AddWithValue("@Car_ID", selectedCar);
+            SqlCommand rentedInsertcmd = Rented_Insert(cnn, Rental_ID, selectedCar);
+
+            SqlCommand transactionInsertcmd = Transaction_Insert(cnn, Rental_ID);
+
+            SqlCommand pickupInsertcmd = Pickup_Insert(cnn, Rental_ID);
 
             SqlCommand command = new SqlCommand("Update IDTracker Set Rental_ID = Rental_ID + 1", cnn);
             //try inserts
@@ -106,6 +99,8 @@ namespace _291CourseProject
             {
                 rentalInsertcmd.ExecuteNonQuery();
                 rentedInsertcmd.ExecuteNonQuery();
+                transactionInsertcmd.ExecuteNonQuery();
+                pickupInsertcmd.ExecuteNonQuery();
                 command.ExecuteNonQuery();
                 var form = new RentalConfirmation();
 
@@ -134,6 +129,59 @@ namespace _291CourseProject
             rdr.Close();
             cnn.Close();
             return Rental_ID[0];
+        }
+
+        private SqlCommand Rental_Insert(SqlConnection cnn ,int Rental_ID)
+        {
+            //insert statment
+            string rentalInsert = "INSERT INTO dbo.Rental(Rental_id,Rental_Type,Pickup_Date,Pickup_time,Return_Date,Return_Time) VALUES(@Rental_id,@Rental_Type,@Pickup_Date,@Pickup_time,@Return_Date,@Return_Time)";
+            //insert values
+            SqlCommand rentalInsertcmd = new SqlCommand(rentalInsert, cnn);
+            rentalInsertcmd.Parameters.AddWithValue("@Rental_ID", Rental_ID);
+            rentalInsertcmd.Parameters.AddWithValue("@Rental_Type", 2);//****************edit************************************************
+            rentalInsertcmd.Parameters.AddWithValue("@Pickup_Date", this.pickupDate);
+            rentalInsertcmd.Parameters.AddWithValue("@Pickup_time", this.pickupTime);
+            rentalInsertcmd.Parameters.AddWithValue("@Return_Date", this.dropoffDate);
+            rentalInsertcmd.Parameters.AddWithValue("@Return_Time", this.dropoffTime);
+
+
+            return rentalInsertcmd;
+        }
+
+        private SqlCommand Rented_Insert(SqlConnection cnn, int Rental_ID, string selectedCar)
+        {
+            //insert statement
+            string rentedInsert = "INSERT INTO dbo.Rented(Rental_ID,Car_ID) VALUES(@Rental_ID,@Car_ID)";
+            //insert values
+            SqlCommand rentedInsertcmd = new SqlCommand(rentedInsert, cnn);
+            rentedInsertcmd.Parameters.AddWithValue("@Rental_ID", Rental_ID);
+            rentedInsertcmd.Parameters.AddWithValue("@Car_ID", selectedCar);
+
+            return rentedInsertcmd;
+        }
+
+        private SqlCommand Transaction_Insert(SqlConnection cnn, int Rental_ID)
+        {
+            //insert statement
+            string transactionInsert = "INSERT INTO dbo.Transactions(Customer_ID,Rental_ID) VALUES(@Customer_ID,@Rental_ID)";
+            //insert values
+            SqlCommand transactionInsertcmd = new SqlCommand(transactionInsert, cnn);
+            transactionInsertcmd.Parameters.AddWithValue("@Customer_ID", this.Customer_ID);
+            transactionInsertcmd.Parameters.AddWithValue("@Rental_ID", Rental_ID);
+
+            return transactionInsertcmd;
+        }
+
+        private SqlCommand Pickup_Insert(SqlConnection cnn, int Rental_ID)
+        {
+            //insert statement
+            string pickupInsert = "INSERT INTO dbo.Pickup(Branch_ID,Rental_ID) VALUES(@Branch_ID,@Rental_ID)";
+            //insert values
+            SqlCommand pickupInsertcmd = new SqlCommand(pickupInsert, cnn);
+            pickupInsertcmd.Parameters.AddWithValue("@Branch_ID", this.selectedBranch);
+            pickupInsertcmd.Parameters.AddWithValue("@Rental_ID", Rental_ID);
+
+            return pickupInsertcmd;
         }
     }
 }
